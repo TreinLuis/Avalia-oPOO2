c2a06a8534431d1447eff34567c9cf52b55bcea3
TreinLuis <21luiseduardo2005@gmail.com>
2024-05-24 07:59:23 -0300

    testing

--- src/app/ACMEMidia.java
+++ src/app/ACMEMidia.java
@@ -2,17 +2,26 @@ package app;
 
 import dados.*;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ACMEMidia {
+    private PrintStream saidaPadrao = System.out;   // Guarda a saida padrao - tela (console)
+    private final String nomeArquivoEntrada = "entrada.txt";  // Nome do arquivo de entrada de dados
+    private final String nomeArquivoSaida = "dadosout.txt";  // Nome do arquivo de
     private Scanner in = new Scanner(System.in);
 
     private Midiateca midiateca;
 
     public ACMEMidia() {
         midiateca = new Midiateca();
+        redirecionaES();
     }
 
     public void executa() {
@@ -241,5 +250,24 @@ public class ACMEMidia {
             }
         }
         return atributo;
+    }
+    private void redirecionaES() {
+        try {
+            BufferedReader streamEntrada = new BufferedReader(new FileReader(nomeArquivoEntrada));
+            in = new Scanner(streamEntrada);   // Usa como entrada um arquivo
+            PrintStream streamSaida = new PrintStream(new File(nomeArquivoSaida), Charset.forName("UTF-8"));
+            System.setOut(streamSaida);             // Usa como saida um arquivo
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        Locale.setDefault(Locale.ENGLISH);   // Ajusta para ponto decimal
+        in.useLocale(Locale.ENGLISH);   // Ajusta para leitura para ponto decimal
+    }
+
+    // Restaura E/S padrao de tela(console)/teclado
+    // Chame este metodo para retornar a leitura e escrita de dados para o padrao
+    private void restauraES() {
+        System.setOut(saidaPadrao);
+        in = new Scanner(System.in);
     }
 }
